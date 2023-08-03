2043bd7996c162fcab4dab185609f0b582cbb55d
darrius <mohamedtine17@gmail.com>
2023-08-03 23:38:50 +0000

    first push

--- App.tsx
+++ App.tsx
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
+import {useState} from 'react'
+
 
 export default function App() {
+  let [verite, setVerite] = useState(false)
+  let [minute, setMinute] = useState(25)
+  
+  const update = () =>{
+    let trustMinute = Math.floor(minute)
+    let totalSecond = trustMinute*60 
+    totalSecond--
+    setMinute(totalSecond/60)  
+  }
+
+  let interval = setInterval(update, 1000)
+  if(verite === true){
+    interval
+    console.log(minute)
+  }else{
+    clearInterval(interval)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View  style={styles.container}>
+      <Text style={styles.title}>This is your new pomodoro</Text>
+      <Button title='On' onPress={()=>setVerite(true)} />
+      <Button  title='Off' onPress={()=>setVerite(false)}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    textAlign:'center',
+    position: 'relative',
+    top:"45%"
   },
+
+  title:{
+    textAlign:'center',
+    marginBottom:'10%'
+  }
 });
